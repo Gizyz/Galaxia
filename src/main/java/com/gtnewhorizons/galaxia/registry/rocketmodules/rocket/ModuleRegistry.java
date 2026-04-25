89464e54dd4d43167979cb3726828b342e9978a6
SkorchedEU <hdgaming700@gmail.com>
2026-04-25 23:27:45 +0100

    Added new engine model, texture, and schematic sprite (#228)

--- src/main/java/com/gtnewhorizons/galaxia/registry/rocketmodules/rocket/ModuleRegistry.java
+++ src/main/java/com/gtnewhorizons/galaxia/registry/rocketmodules/rocket/ModuleRegistry.java
@@ -38,7 +38,7 @@ public final class ModuleRegistry {
         new FuelTankModule(1, "Basic Fuel Tank", 5.0, 3.0, 1200.0, "fuel_tank_3x5x3", 8000.0);
         new CapsuleModule(2, "Basic Capsule Module", 2.5, 3.0, 450.0, "capsule_3x2.5x3", -1.75, 1);
         new StorageModule(3, "Basic Storage Module", 4.0, 3.0, 900.0, "storage_unit_3x4x3", 100);
-        new EngineModule(4, "Basic Fuel Engine", 0.5, 3.0, 250.0, "engine_3x1x3", 6000.0);
+        new EngineModule(4, "Basic Fuel Engine", 3.47, 3.0, 250.0, "engine_3x1.75x3", 6000.0);
         new LanderModule(5, "Basic Lander", 2.5, 3.0, 250.0, "capsule_3x2.5x3", -1.75, 1);
         // Add new tiers in same format below:
     }
